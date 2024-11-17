e153a3955376643d451f89ca2e9d8314733c2d96
Dadash Huseynzade <huseynzade.dadas@gmail.com>
2024-11-17 17:03:32 +0400

    feat: add application form to details component and implement submission logic

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -114,4 +114,8 @@ export class HousingService {
   getHousingLocationById(id: number): HousingLocation | undefined {
     return this.housingLocationList.find((housingLocation) => housingLocation.id === id);
   }
+
+  submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(`Application submitted for ${firstName} ${lastName} with email ${email}`);
+  }
 }
