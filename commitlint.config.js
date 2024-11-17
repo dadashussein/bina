dd1c3cba8ef7fc0715220bfbfc8fd3219bdcbc1f
Dadash Huseynzade <huseynzade.dadas@gmail.com>
2024-11-17 14:15:03 +0400

    chore: update commitlint configuration to enforce rules for types and scopes

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,21 @@
 module.exports = {
     extends: ['@commitlint/config-angular'],
-  };
+    rules: {
+        'type-enum': [
+            2,
+            'always',
+            ['feat', 'fix', 'docs', 'hot', 'style', 'refactor', 'perf', 'test', 'chore', 'revert', 'migrate']
+        ],
+        'scope-enum': [
+            2,
+            'always',
+            [
+                //Package Jsonla bagli degisiklikler
+                'package',
+                //Componentlerle ilgili degisiklikler
+                'component',
+            ]
+        ],
+        'scope-empty': [2, 'never']
+    }
+};
